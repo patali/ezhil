877f597aa02ea3d1136f26bc2f83775bd8c3a7dd
Patali DO <sharath.patali@gmail.com>
2019-12-29 12:47:42 +0000

    Updated js to check if a comment was submitted

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,10 @@
-function changeValue(elementName, newValue){
-  document.getElementsByName(elementName)[0].value=newValue;
-  window.location.hash = "#comment-form";
-};
-
-if(window.location.hash) {
-  var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
-  alert (hash);
+function checkCommentPosted() {
+  if(window.location.hash) {
+    var hash = window.location.hash.substring(1);
+    if(hash === "comment-submitted") {
+      alert("Thank you for posting a comment! Your comment is now under review.");
+    }
+  }
 }
+
+window.onload = checkCommentPosted;
