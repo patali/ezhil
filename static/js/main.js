c836cdb29276ae3ff09aad1fbe688186e4dd5c34
Patali DO <sharath.patali@gmail.com>
2019-12-29 05:41:08 +0000

    Added alert message after comment is sent

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,8 @@ function changeValue(elementName, newValue){
   document.getElementsByName(elementName)[0].value=newValue;
   window.location.hash = "#comment-form";
 };
+
+if(window.location.hash) {
+  var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+  alert (hash);
+}
